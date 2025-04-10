85e6329720f2a1adc4c2652a985a306dbacec95f
Rudolf Pliva <rpliva@seznam.cz>
2025-04-10 22:33:46 +0200

    device support

--- custom_components/evmate/sensor.py
+++ custom_components/evmate/sensor.py
@@ -4,12 +4,16 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+)
 from homeassistant.components.sensor import (
     SensorEntity,
     SensorEntityDescription,
 )
 
-from .const import DOMAIN, SENSOR_TYPES
+from .const import BINARY_SENSOR_TYPES, DOMAIN, SENSOR_TYPES
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
@@ -26,6 +30,9 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the sensor platform."""
+    device = EVMateDevice(coordinator=entry.runtime_data.coordinator)
+    async_add_entities(device)
+
     async_add_entities(
         EVMateSensor(
             entry.unique_id
@@ -33,26 +40,43 @@ async def async_setup_entry(
             + entity_description.key.replace(",", "_").replace(" ", "_"),
             coordinator=entry.runtime_data.coordinator,
             entity_description=entity_description,
+            device=device,
         )
         for entity_description in SENSOR_TYPES
     )
 
+    async_add_entities(
+        EVMateBinarySensor(
+            entry.unique_id
+            + "-"
+            + entity_description.key.replace(",", "_").replace(" ", "_"),
+            coordinator=entry.runtime_data.coordinator,
+            description=entity_description,
+            device=device,
+        )
+        for entity_description in BINARY_SENSOR_TYPES
+    )
 
-class EVMateSensor(SensorEntity):
-    """evmate Sensor class."""
+
+class EVMateDevice(SensorEntity):
+    """EVMate device class."""
 
     def __init__(
         self,
-        unique_id: str,
-        entity_description: SensorEntityDescription,
         coordinator: EVMateDataUpdateCoordinator,
     ) -> None:
         """Initialize the sensor class."""
         super().__init__()
-        self.entity_description = entity_description
+        self.entity_description = SensorEntityDescription(
+            key="ID",
+            name="Serial number",
+        )
         self._coordinator = coordinator
-        self._attr_name = "EVMate"  # entity_description.name
-        self._attr_unique_id = unique_id
+        self.serial_number = self._coordinator.data.get(
+            self.entity_description.key, None
+        )
+        self._attr_name = "EVMate IoTMeter"
+        self._attr_unique_id = DOMAIN + "_" + self.serial_number
 
     @property
     def device_info(self) -> any:
@@ -64,7 +88,7 @@ class EVMateSensor(SensorEntity):
             "sw_version": self._coordinator.data.get("txt,ACTUAL SW VERSION", None),
             "model": "IoTMeter",
             "manufacturer": "EVMate",
-            "serial_number": self._coordinator.data.get("ID", None),
+            "serial_number": self.serial_number,
         }
 
     @property
@@ -86,3 +110,91 @@ class EVMateSensor(SensorEntity):
     def native_value(self) -> StateType:
         """Return the state of the device."""
         return self._coordinator.data.get(self.entity_description.key, None)
+
+
+class EVMateSensor(SensorEntity):
+    """evmate Sensor class."""
+
+    def __init__(
+        self,
+        unique_id: str,
+        entity_description: SensorEntityDescription,
+        coordinator: EVMateDataUpdateCoordinator,
+        device: EVMateDevice,
+    ) -> None:
+        """Initialize the sensor class."""
+        super().__init__()
+        self.entity_description = entity_description
+        self.device = device
+        self._coordinator = coordinator
+        self._attr_name = entity_description.name
+        self._attr_unique_id = unique_id
+
+    @property
+    def device_info(self):  # noqa: ANN201
+        """Return information to link this entity with the correct device."""
+        return {"identifiers": {(DOMAIN, self.device._attr_unique_id)}}  # noqa: SLF001
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._coordinator.last_update_success
+
+    async def async_added_to_hass(self) -> None:
+        """Connect to dispatcher listening for entity data notifications."""
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
+    async def async_update(self) -> None:
+        """Get the latest data from OWM and updates the states."""
+        await self._coordinator.async_request_refresh()
+
+    @property
+    def native_value(self) -> StateType:
+        """Return the state of the device."""
+        return self._coordinator.data.get(self.entity_description.key, None)
+
+
+class EVMateBinarySensor(BinarySensorEntity):
+    """EVMate Binary sensor class."""
+
+    def __init__(
+        self,
+        unique_id: str,
+        description: BinarySensorEntityDescription,
+        coordinator: EVMateDataUpdateCoordinator,
+        device: EVMateDevice,
+    ) -> None:
+        """Initialize the Binary sensor class."""
+        super().__init__()
+        self.entity_description = description
+        self.device = device
+        self._coordinator = coordinator
+        self._attr_name = description.name
+        self._attr_unique_id = unique_id
+
+    @property
+    def device_info(self):  # noqa: ANN201
+        """Return information to link this entity with the correct device."""
+        return {"identifiers": {(DOMAIN, self.device._attr_unique_id)}}  # noqa: SLF001
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._coordinator.last_update_success
+
+    async def async_added_to_hass(self) -> None:
+        """Connect to dispatcher listening for entity data notifications."""
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self.async_write_ha_state)
+        )
+
+    async def async_update(self) -> None:
+        """Get the latest data from OWM and updates the states."""
+        await self._coordinator.async_request_refresh()
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the binary sensor."""
+        return self._coordinator.data.get(self.entity_description.key, "0") == "1"
